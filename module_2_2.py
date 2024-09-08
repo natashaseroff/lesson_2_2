bc8f96ea380a575c5afd95e69d8a90d9af934c37
natashaseroff <natashaseroff@mail.ru>
2024-09-08 20:11:25 +0200

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 15
+second = 20
+third = 25
+if first == second and first == third and second == third:
+        print(3)
+elif first == second or first == third or second == third:
+        print(2)
+else :
+        print(0)
